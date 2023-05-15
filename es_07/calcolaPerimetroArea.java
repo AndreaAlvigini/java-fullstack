c7035cb3d6f7ee562ab4480a56f98d88a9af8f4e
AndreaAlvigini <andreaalvigini@gmail.com>
2023-05-15 10:41:16 +0200

    01 creazione Classe e Metodo

---
+++ es_07/calcolaPerimetroArea.java
@@ -0,0 +1,15 @@
+/*
+Creare un programma che chieda all'utente di inserire la lunghezza del latoe restituisce in output
+l'area e il perimetro del quadratp utilizzando la variabile lato
+*/
+
+public class calcolaPerimetroArea { // classe chiamata come nome file
+
+  public static void main(String[] args) {
+    //le istruzioni vanno qui, nel metodo main
+
+  }
+}
+/*
+  Note per collaboratori 
+  */
