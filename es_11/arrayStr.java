ab416cb0c4d13c25b3f1de3d25ec3a946c8c6315
AndreaAlvigini <andreaalvigini@gmail.com>
2023-05-16 09:28:10 +0200

    Upload arrayStr

---
+++ es_11/arrayStr.java
@@ -0,0 +1,16 @@
+public class arrayStr
+{
+     public static void main(String[] args) 
+   {
+      String[] amici = {"Gianluca", "Davide", "Emanuele", "Tiziano"};
+    
+      for (String amico : amici) 
+       {
+        System.out.println(amico);
+
+       }
+    }
+  }
+ 
+  
+
